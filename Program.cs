58733dbe87f71678ac28ea9e681e5202e920da05
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-06-01 16:20:34 +0700

    lan1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sosanhten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string[] student = { "Huy", "Cường", "Đạt", "Quân", "Hà", "Quốc Anh" };
+            Console.WriteLine("nhập tên sinh viên cần tìm: ");
+            string ten = Console.ReadLine();
+            bool tontai = false;
+            for (int i = 0; i < student.Length; i++)
+            {
+                if (student[i].Equals(ten))
+                {
+                    Console.WriteLine("Vi tri hoc sinh " + ten + " la " + (i + 1));
+                    tontai = true;
+                    break;
+                }
+            }
+            if (!tontai)
+            {
+                Console.WriteLine("khong tim thay ten phu hop");
+            }
+            Console.ReadKey();
+        }
+            
+    }
+}
